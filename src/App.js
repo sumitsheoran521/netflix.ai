d060353a7c04cf30844fdf9a035ab90060f96e7b
sumitsheoran521 <sumitsheoran521@gmail.com>
2025-03-11 13:13:12 +0530

    built signin/signup form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      Namaste everyone, This is Netflix-GPT
-    </div>
+    <Body />
   );
 }
 
